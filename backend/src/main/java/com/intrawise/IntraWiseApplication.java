940fda066e046cb884d0eaa7430f3e16ce7674d7
AryalAyush09 <aryalayush34@gmail.com>
2025-09-12 12:35:02 +0545

    chore: initialize IntraWise Secure Assistant project with Spring Boot setup

---
+++ backend/src/main/java/com/intrawise/IntraWiseApplication.java
@@ -0,0 +1,12 @@
+package com.intrawise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntraWiseApplication.class, args);
+	}
+}
